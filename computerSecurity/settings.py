62a10351dcdde29f400cfec83931b0c0dcb781c7
Noa Mishraki <noamishraki2@gmail.com>
2021-12-29 22:03:21 +0200

    add ssl

--- computerSecurity/settings.py
+++ computerSecurity/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'webApp',
     'crispy_forms',
 
