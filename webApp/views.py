3fd3874fe4c61f5433568219dab3bddff7244ea1
Noa Mishraki <noamishraki2@gmail.com>
2021-12-29 21:23:01 +0200

    WORK!

--- webApp/views.py
+++ webApp/views.py
@@ -1,28 +1,25 @@
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 from django.http import response
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from webApp.core import is_input_text_valid
-from .forms import RegisterForm , PasswordChangingForm
+from .forms import RegisterForm, PasswordChangingForm
 from webApp.models import Costumer
 from django.contrib.auth.decorators import login_required
 
 
-
-
 @login_required(login_url='/login/')
 def index(response):
-
     if response.method == 'POST':
         if response.POST.get("submit"):
-            if not is_input_text_valid(response.POST.get('costumer_name'),256):
+            if not is_input_text_valid(response.POST.get('costumer_name'), 256):
                 messages.success(response, f'Costumer name is invalid')
                 return redirect('/')
-            elif not is_input_text_valid(response.POST.get('costumer_email'),256):
+            elif not is_input_text_valid(response.POST.get('costumer_email'), 256):
                 messages.success(response, f'Costumer email is invalid')
                 return redirect('/')
-            elif not is_input_text_valid(response.POST.get('costumer_info'),256):
+            elif not is_input_text_valid(response.POST.get('costumer_info'), 256):
                 messages.success(response, f'Costumer info is invalid')
                 return redirect('/')
 
@@ -42,7 +39,6 @@ def index(response):
                     messages.success(response, f'Costumer {costumer_name} was deleted')
 
         return redirect('/')
-    
 
     all_costumers = Costumer.objects.all()
     relevant_costumers = [costumer for costumer in all_costumers if costumer.sales_person == response.user]
@@ -53,28 +49,27 @@ def index(response):
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
-        print(f"respone {response.POST}",flush=True)
+        print(f"respone {response.POST}", flush=True)
         if form.is_valid():
             new_user = form.save()
             new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],)
+                                    password=form.cleaned_data['password1'], )
             login(response, new_user)
 
             return redirect("/")
     else:
         form = RegisterForm()
 
-    return render(response, "registration/register.html", {"form":form})
+    return render(response, "registration/register.html", {"form": form})
 
 
-    
 def password_change(response):
     if response.method == "POST":
         try:
             form = PasswordChangingForm(response.POST)
-            print(f"respone {response.POST}",flush=True)
+            print(f"respone {response.POST}", flush=True)
             print(response.user, flush=True)
-            new_user = authenticate(username=response.user,password=response.POST.get('new_password'),)
+            new_user = authenticate(username=response.user, password=response.POST.get('new_password'), )
             login(response, new_user)
             messages.success(response, f'Password changed succuessfuly. Please go back to main page')
         except:
@@ -84,14 +79,9 @@ def password_change(response):
     else:
         form = PasswordChangingForm(response.POST)
 
-    return render(response, "registration/change-password.html", {"form":form})
+    return render(response, "registration/change-password.html", {"form": form})
 
 
 def logout_user(request):
-	logout(request)
-	return redirect('login')
-
-
-
-
-
+    logout(request)
+    return redirect('login')
